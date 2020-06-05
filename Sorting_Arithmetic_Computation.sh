5996a4002f0830c5ae6a9862bc155caadcaaee17
“Aniket_D” <aniketdakhare@gmail.com>
2020-06-05 16:03:32 +0530

    Sorted the Result in Descending Order

--- Sorting_Arithmetic_Computation.sh
+++ Sorting_Arithmetic_Computation.sh
@@ -7,6 +7,12 @@ read -p "Enter value of 'b'  " b
 read -p "Entar value if 'c'  " c
 echo "a=$a b=$b c=$c"
 
+declare -A compute
+compute[exp"0"]=$(($a+$b*$c))
+compute[exp"1"]=$(($a*$b+$c))
+compute[exp"2"]=$(($c+$a/$b))
+compute[exp"3"]=$(($a%$b+$c))
+
 i=0
 for key in ${!compute[@]}
 do
