0d02423b59f60b5a381e7fdd4708e9c96ddc94d0
“Aniket_D” <aniketdakhare@gmail.com>
2020-06-05 15:05:34 +0530

    Reading the values

--- Sorting_Arithmetic_Computation.sh
+++ Sorting_Arithmetic_Computation.sh
@@ -1,1 +1,9 @@
+#!/bin/bash -x
+
 echo Welcome to Arithmetic Computation and Sorting
+
+read -p "Enter value of 'a'  " a
+read -p "Enter value of 'b'  " b
+read -p "Entar value if 'c'  " c
+echo "a=$a b=$b c=$c"
+
